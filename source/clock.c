d10cab8521ee147d1619c2f6078383820e5f456b
Joel <sauskethunderkid@gmail.com>
2016-04-28 23:43:33 -0500

    Fixed compile errors

--- source/clock.c
+++ source/clock.c
@@ -1,31 +1,33 @@
 #include "clock.h"
 #include "homeMenu.h"
 
+u64 lastTimeInSeconds = 0;
+
 void digitalTime(int x, int y)
 {
-	bool hour24 = false; //Until I add an option this will be default
+	if(lastTimeInSeconds == 0) 
+	{
+		lastTimeInSeconds = osGetTime() / 1000; //get on boot.
+	}
+	u64 timeInSeconds = osGetTime() / 1000;
+
+	lastTimeInSeconds = timeInSeconds;
 	
+	u64 convert = ( (70*365+17) * 86400LLU );
+	time_t now = timeInSeconds- convert;
 	struct tm *ts = localtime(&now);
 
 	int hours = ts->tm_hour;
 	int minutes = ts->tm_min;
-
-    	char * state;
-
 	
-
 	sftd_draw_textf(roboto, x, y, RGBA8(255, 255, 255, 255), 12, "%2d:%02d", hours, minutes);
-	
-	if (hour24) state = "";
-  	else 
-  	{
-        	if (hours > 12)
-			sftd_draw_textf(roboto, x+30, y+2, RGBA8(255, 255, 255, 255), 10, "PM");
-		else 
-			sftd_draw_textf(roboto, x+30, y+2, RGBA8(255, 255, 255, 255), 10, "AM");
-        	if (hour > 12) 
-        		hour -= 12;
-    	}
+
+    if (hours > 12)
+		sftd_draw_textf(roboto, x+30, y+2, RGBA8(255, 255, 255, 255), 10, "PM");
+	else 
+		sftd_draw_textf(roboto, x+30, y+2, RGBA8(255, 255, 255, 255), 10, "AM");
+       	if (hours > 12) 
+       		hours -= 12;
 }
 
 void getMonthOfYear(int x, int y, int size)
@@ -34,7 +36,17 @@ void getMonthOfYear(int x, int y, int size)
 	{
 		"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
 	};
-       
+	
+	if(lastTimeInSeconds == 0) 
+	{
+		lastTimeInSeconds = osGetTime() / 1000; //get on boot.
+	}
+	u64 timeInSeconds = osGetTime() / 1000;
+
+	lastTimeInSeconds = timeInSeconds;
+	
+	u64 convert = ( (70*365+17) * 86400LLU );
+	time_t now = timeInSeconds- convert;
 	struct tm *ts = localtime(&now);
 	int month = ts->tm_mon;
 	int day = ts->tm_mday;
