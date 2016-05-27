e4af0af95420a68d61c3f545257ea6d7268fdfd9
Joel <sauskethunderkid@gmail.com>
2016-05-27 16:29:46 -0400

    We don't need libogg or libvorbis for now

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -23,17 +23,12 @@ wget http://download.savannah.gnu.org/releases/freetype/freetype-2.5.4.tar.bz2
 wget http://sourceforge.net/projects/libjpeg-turbo/files/1.3.1/libjpeg-turbo-1.3.1.tar.gz
 wget http://prdownloads.sourceforge.net/libpng/libpng-1.6.15.tar.xz
 wget http://prdownloads.sourceforge.net/libpng/zlib-1.2.8.tar.gz
-wget http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.xz
-wget http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.xz
 
 make zlib
 make install-zlib
-make libogg
-make install # Need to install libogg for libvorbis
 make freetype
 make libpng
 make libjpeg-turbo
-make libvorbis
 make install
 
 cd -
