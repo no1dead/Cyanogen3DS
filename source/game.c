84e432c7a8b74ca6e13a580b3f0c8e52a56b2b06
Joel <sauskethunderkid@gmail.com>
2016-07-11 16:15:01 -0400

    Merge all upstream changes pertaining to game launcher

--- source/game.c
+++ source/game.c
@@ -4,6 +4,87 @@
 /*These are mostly based off of rinnegatamte's lua player plus src code. 
 I will alter these to my needs later.*/
 
+bool isNinjhax2(void)
+{
+	Result ret = hbInit();
+	if(!ret)
+	{
+		hbExit();
+		return false;
+	}else return true;
+}
+
+extern void (*__system_retAddr)(void);
+
+static Handle hbFileHandle;
+static u32 argbuffer[0x200];
+static u32 argbuffer_length = 0;
+
+// ninjhax 1.x
+void (*callBootloader_1x)(Handle hb, Handle file);
+void (*setArgs_1x)(u32* src, u32 length);
+
+static void launchFile_1x(void)
+{
+	// jump to bootloader
+	callBootloader_1x(0x00000000, hbFileHandle);
+}
+
+// ninjhax 2.0+
+typedef struct
+{
+	int processId;
+	bool capabilities[0x10];
+}processEntry_s;
+
+void (*callBootloader_2x)(Handle file, u32* argbuf, u32 arglength) = (void*)0x00100000;
+void (*callBootloaderNewProcess_2x)(int processId, u32* argbuf, u32 arglength) = (void*)0x00100008;
+void (*callBootloaderRunTitle_2x)(u8 mediatype, u32* argbuf, u32 argbuflength, u32 tid_low, u32 tid_high) = (void*)0x00100010;
+void (*getBestProcess_2x)(u32 sectionSizes[3], bool* requirements, int num_requirements, processEntry_s* out, int out_size, int* out_len) = (void*)0x0010000C;
+
+int targetProcessId = -1;
+titleInfo_s  target_title;
+
+static void launchFile_2x(void)
+{
+	// jump to bootloader
+	if(targetProcessId == -1)callBootloader_2x(hbFileHandle, argbuffer, argbuffer_length);
+	else if(targetProcessId == -2)callBootloaderRunTitle_2x(target_title.mediatype, argbuffer, argbuffer_length, target_title.title_id & 0xffffffff, (target_title.title_id >> 32) & 0xffffffff);
+	else callBootloaderNewProcess_2x(targetProcessId, argbuffer, argbuffer_length);
+}
+
+int launch3DSX(const char * path)
+{
+	if (isNinjhax2())
+	{
+		argbuffer[0] = 1;
+		snprintf((char*)&argbuffer[1], sizeof(argbuffer) - 4, "sdmc:%s", path);
+		argbuffer_length = 0x100;
+	}
+	else 
+	{
+		HB_GetBootloaderAddresses((void**)&callBootloader_1x, (void**)&setArgs_1x);
+		hbExit();
+	}
+	
+	openSdArchive();
+	Result ret = FSUSER_OpenFileDirectly(&hbFileHandle, ARCHIVE_SDMC,  fsMakePath(PATH_EMPTY, ""), fsMakePath(PATH_ASCII, path), FS_OPEN_READ, 0x00000000);
+		if (ret) 
+			return 0;
+	if (isNinjhax2()) 
+		__system_retAddr = launchFile_2x;
+	else
+	{
+		static u32 argbuffer_nh1[0x200];
+		argbuffer_nh1[0]=1;
+		snprintf((char*)&argbuffer_nh1[1], 0x800, "sdmc:%s", path);
+		setArgs_1x(argbuffer_nh1, 0x800);
+		__system_retAddr = launchFile_1x;
+	}
+	
+	return 0;
+}
+
 int launchApp(u64 titleId) 
 {
 	char product_id[16];
--- source/game.h
+++ source/game.h
@@ -9,8 +9,14 @@
 /*These are mostly based off of rinnegatamte's lua player plus src code. 
 I will alter these to my needs later.*/
 
+typedef struct
+{
+	u8 mediatype;
+	u64 title_id;
+}titleInfo_s;
+
+int launch3DSX(const char * path);
 int launchApp(u64 titleId);
 char * getCardID();
-int launch3DSX(char * path);
 int installCIA(const char *path);
 int launchCia(u64 titleID, FS_MediaType mediaType);
